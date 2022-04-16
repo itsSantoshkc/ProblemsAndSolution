4bfee605ce5bbc75ce42a53b3e424cae5a50ed4b
Santosh kc <santoshkc7786@gmail.com>
2022-04-16 18:45:38 +0545

    Commit

--- Array Challenges/RecordBreaker.cpp
+++ Array Challenges/RecordBreaker.cpp
@@ -73,28 +73,27 @@ using namespace std;
 int main(){
    int n;
    cin>>n;
-   int a[n];
-   for(int i  = 0;i<n;i++){
-       cin>>a[i];
-   }
+    int a[n+1];
 
-   int ans = 2;
-   int pd = a[1] - a[0];
-   int j = 2;
-   int curr = 2;
+    for(int i = 0;i<n ;i++){
+        cin >> a[i];
+    }
+    a[n] = -1;
+    if(n == 1){
+        cout << "1"<< endl;
+        return;
+    }
 
-   while(j<n)
-   {
-        if(pd == a[j] - a[j - 1]){
-            curr ++;
-        }else{
-            pd = a[j] - a[j-1];
-            curr = 2;
+    int ans = 0;
+
+    int mx = 1;
+
+    for(int i = 0;i < n; i++){
+        if(a[i]>mx && a[i]>a[i + 1]){
+            ans++;
         }
-        ans = max(ans,curr);
-       j++;
-   }
-   cout<< ans;
+        mx = max(mx,a[i]);
+    }
+    cout << ans <<endl;
 
-    return 0;
 }
